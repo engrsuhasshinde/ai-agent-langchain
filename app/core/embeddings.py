ca5453170c9642fca5c4f89e0465f05ab3edfcd1
Suhas Shinde <engr.suhas.shinde@gmail.com>
2026-05-29 04:53:00 +0530

    Fix: Replace manual embeddings

--- app/core/embeddings.py
+++ app/core/embeddings.py
@@ -1,10 +1,12 @@
 from app.core.llm import client
 from app.config.settings import settings
+from langchain_openai import OpenAIEmbeddings
+
+embeddings_model = OpenAIEmbeddings(
+    model = settings.EMBEDDING_MODEL,
+    openai_api_base = settings.BASE_URL,  # points to local Ollama server
+    openai_api_key = settings.API_KEY
+)
 
 def get_embedding(text: str) -> list[float]:
-    response = client.embeddings.create(
-        model=settings.EMBEDDING_MODEL,
-        input=text
-    )
-
-    return response.data[0].embedding
+    return embeddings_model.embed_query(text)
